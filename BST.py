19f074ed6cea16347553e0b6c904c288c3baec16
Jaxon Johnson <88282807+WaffleKone@users.noreply.github.com>
2022-09-24 12:51:44 -0400

    added add and addnode funcs

--- BST.py
+++ BST.py
@@ -13,6 +13,7 @@ class BST():
   def __init__(self, root=None):
     self.root = root
     self.output = ""
+    self.contents = []
     
   def print_tree(self, node, level=0):
     if node != None:
@@ -26,8 +27,47 @@ class BST():
     else:
       self.print_tree(self.root)
       return (self.output)
+    def __repr__(self):
+      if self.root is None:
+        return("The tree is empty")
+      else:
+        self.print_tree(self.root)
+        return (self.output)
   #Part 3: Add functionality to your BST class
+  
+  def add(self, input):
+    if type(input) != int and type(input) != BSTNode:
+      raise ValueError("Invalid input, please input either an integer or a BSTNode.")
+    
+    if type(input) == int:
+      BSTNode(input)
+    
+    if input.data in self.contents:
+      return
 
+    if self.root == None:
+      self.root == input
+      self.contents.append(input.data)
+      return
+
+    self.add_node(self.root, input)
+  
+  def add_node(self, c_node, n_node):
+    if n_node.data > c_node.data:
+      if c_node.right == None:
+        c_node.right = n_node
+        self.contents.append(n_node.data)
+        return
+      else:
+        self.add_node(c_node.right, n_node)
+    else:
+      if c_node.left == None:
+        c_node.left = n_node
+        self.contents.append(n_node.data)
+        return
+      else:
+        self.add_node(c_node.left, n_node)
+    
 
 
 # Testing
@@ -49,3 +89,24 @@ print(bst)
 
 node2.right = node3
 print(bst)
+#create tree from image
+node8 = BSTNode(8)
+node3 = BSTNode(3)
+node10 = BSTNode(10)
+node1 = BSTNode(1)
+node6 = BSTNode(6)
+node14 = BSTNode(14)
+node4 = BSTNode(4)
+node7 = BSTNode(7)
+node13 = BSTNode(13)
+
+bst = BST()
+bst.add(node8)
+bst.add(node3)
+bst.add(node10)
+bst.add(node1)
+bst.add(node6)
+bst.add(node14)
+bst.add(node4)
+bst.add(node7)
+bst.add(node13)
